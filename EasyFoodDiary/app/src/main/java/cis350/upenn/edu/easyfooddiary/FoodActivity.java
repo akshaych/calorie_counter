e64d4e63ac6f50d8aa900e61ab2752196232f5a6
haile1996 <willchen@sas.upenn.edu>
2017-02-24 20:47:48 -0500

    Finished
    Finished Calendar + Food

--- EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/FoodActivity.java
+++ EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/FoodActivity.java
@@ -39,31 +39,48 @@ public class FoodActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
         foodView = new FoodView(this);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myref_date = database.getReference(date);
+        myref_date.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String s = dataSnapshot.getValue(String.class);
+                try {
+                    dateInfo = new JSONArray(s);
+                } catch (JSONException e) {
+                    Toast.makeText(FoodActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("tag", "Failed to read value.", error.toException());
+            }
+        });
+        editText_weight = (EditText) findViewById(R.id.weight);
+        editText_breakfast = (EditText) findViewById(R.id.breakfast);
+        editText_lunch = (EditText) findViewById(R.id.lunch);
+        editText_dinner = (EditText) findViewById(R.id.dinner);
+        editText_snack = (EditText) findViewById(R.id.snack);
+
+        try {
+            editText_weight.setText((String) dateInfo.get(0));
+            editText_breakfast.setText((String) dateInfo.get(1));
+            editText_lunch.setText((String) dateInfo.get(2));
+            editText_dinner.setText((String) dateInfo.get(3));
+            editText_snack.setText((String) dateInfo.get(4));
+        } catch (JSONException e) {
+            Toast.makeText(FoodActivity.this, "Error", Toast.LENGTH_SHORT).show();
+        }
     }
 
     protected void onClick(View view) {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myref_date = database.getReference(date);
-
-        myref_date.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String s = dataSnapshot.getValue(String.class);
-                try {
-                    dateInfo = new JSONArray(s);
-                } catch (JSONException e) {
-                    Toast.makeText(FoodActivity.this,"Error", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                Log.w("tag", "Failed to read value.", error.toException());
-            }
-        });
-
-        DatabaseReference myref_dateInfo = database.getReference(dateInfo.toString());
 
         editText_weight = (EditText) findViewById(R.id.weight);
         weight = editText_weight.getText().toString();
@@ -80,26 +97,16 @@ public class FoodActivity extends AppCompatActivity {
         editText_snack = (EditText) findViewById(R.id.snack);
         snack = editText_snack.getText().toString();
 
-        myref_dateInfo.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.v("tag", "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("tag", "Failed to read value.", error.toException());
-            }
-        });
-
-        myref_name.setValue(namer);
-        myref_date.setValue(birthdate);
-        myref_goal.setValue(goal);
-
+        try {
+            dateInfo.put(0, weight);
+            dateInfo.put(1, breakfast);
+            dateInfo.put(2, lunch);
+            dateInfo.put(3, dinner);
+            dateInfo.put(4, snack);
+        } catch (JSONException e) {
+            Toast.makeText(FoodActivity.this, "Error", Toast.LENGTH_SHORT).show();
+        }
+        myref_date.setValue(dateInfo);
 
         Toast.makeText(foodView.getContext(),
                 "Saved", Toast.LENGTH_SHORT).show();
