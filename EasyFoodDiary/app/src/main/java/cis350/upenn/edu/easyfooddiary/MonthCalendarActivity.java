bef0dde280494922805e38abebac90acdf8de928
Daniel Trinh <dantrinh@seas.upenn.edu>
2017-03-24 22:33:39 -0400

    Updating MonthCalendarActivity
    Update of monthly calendar graph of weights feature

--- EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/MonthCalendarActivity.java
+++ EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/MonthCalendarActivity.java
@@ -3,12 +3,22 @@ package cis350.upenn.edu.easyfooddiary;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -18,19 +28,61 @@ import java.util.Date;
  */
     public class MonthCalendarActivity extends AppCompatActivity {
 
+        protected int day = 1;
+        protected int[] weights;
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_monthly_calendar);
+
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
             Calendar calendar = Calendar.getInstance();
+            //get days in that month
+            int days = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+            weights = new int[days];
             Date d1 = calendar.getTime();
-            
+            int month = d1.getMonth();
+
+            while (day <= days) {
+                DatabaseReference myref_year = database.getReference(day + "," + month + ",2017");
+                myref_year.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String s = dataSnapshot.getValue(String.class);
+                        try {
+                            if (s == null) {
+                                weights[day - 1] = -1;
+                            } else {
+                                weights[day - 1] = Integer.parseInt((String) (new JSONArray(s)).get(0));
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(MonthCalendarActivity.this, "Error1", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w("tag", "Failed to read value.", error.toException());
+                    }
+                });
+                day++;
+            }
+
+
             GraphView graph = (GraphView) findViewById(R.id.graph);
-            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                    new DataPoint(0, 1),
-                    new DataPoint(1, 5),
-                    new DataPoint(2, 3)
-            });
+            DataPoint[] arr = new DataPoint[days];
+
+            for (int i = 0; i < days; i++) {
+                if (weights[i] >= 0) {
+                    arr[i] = new DataPoint(i, weights [i]);
+                }
+            }
+
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(arr);
             graph.addSeries(series);
 
             series.setTitle("Current Month Calories");
@@ -40,13 +92,8 @@ import java.util.Date;
         }
 
         public void onClick(View view) {
-            //have to differentiate amongst buttons
-            if (view.getId() == R.id.your_information) {
-                Intent i = new Intent(this, InformationActivity.class);
-                startActivity(i);
-            }
-            else if (view.getId() == R.id.calendar) {
-                Intent i = new Intent(this, CalendarActivity.class);
+            if (view.getId() == R.id.main) {
+                Intent i = new Intent(this, MainActivity.class);
                 startActivity(i);
             }
         }
