48128e73df50fa342d10f4850b3cd48f01eb888e
Vamsee <vamsee@m.com>
2017-04-07 20:09:26 -0400

    Fixed Akshay's nutrition activity

---
+++ EasyFoodDiary/app/src/main/java/cis350/upenn/edu/easyfooddiary/NutritionView.java
@@ -0,0 +1,21 @@
+package cis350.upenn.edu.easyfooddiary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by akshay on 4/7/17.
+ */
+
+public class NutritionView extends TextView {
+
+    public NutritionView(Context c) {
+        super(c);
+    }
+
+    public NutritionView(Context c, AttributeSet a) {
+        super(c, a);
+    }
+
+}
